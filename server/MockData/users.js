5beee3419319fdc80dd194d82463069d5e278bd0
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-15 15:25:40 +0300

    ft(api): add users mockdata [ #167285201]

---
+++ server/MockData/users.js
@@ -0,0 +1,20 @@
+const users = [
+	{
+		id: 1,
+		firstname: 'main',
+		lastname: 'admin',
+		email: 'admin123@gmail.com',
+		password: '$2b$10$RimQIoicGM6VRa8zGCgNlOeiimxeBllhyfPLE7OfSo1blRlTY4wGC',
+		isAdmin: true,
+	},
+	{
+		id: 2,
+		firstname: 'Joseph',
+		lastname: 'Njuguna',
+		email: 'josephnjuguna482@gmail.com',
+		password: '$2b$10$RimQIoicGM6VRa8zGCgNlOeiimxeBllhyfPLE7OfSo1blRlTY4wGC',
+		isAdmin: false,
+	},
+];
+
+export default users;
