d39ae26a985304bc29b78011a22bb6e86ea9a6ad
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-15 15:18:52 +0300

    ft(api): add user signup models function [#167285201]

---
+++ server/models/Users.js
@@ -0,0 +1,37 @@
+import db from '../MockData/users';
+
+class UsersModel {
+	constructor(payload = null) {
+		this.payload = payload;
+		this.result = null;
+	}
+
+	static async findByEmail(email) {
+		const obj = db.find(o => o.email === email);
+		if (!obj) {
+			return false;
+		}
+		this.result = obj;
+		return true;
+	}
+
+	async signup() {
+		const userid = db.length + 1;
+		const user = {
+			id: userid,
+			firstname: this.payload.firstname,
+			lastname: this.payload.lastname,
+			email: this.payload.email,
+			password: this.payload.hashedPassword,
+			isAdmin: false,
+		};
+		const obj = db.find(o => o.email === this.payload.email);
+		if (!obj) {
+			db.push(user);
+			this.result = user;
+			return true;
+		}
+	}
+}
+
+export default UsersModel;
