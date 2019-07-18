b1a2345b167b73dbead4d12559c7b5555313b993
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-18 14:55:34 +0300

    ft(view-all-trips): update view trips controller function  [Finishes  #167285198]

--- server/controllers/Trips.js
+++ server/controllers/Trips.js
@@ -42,15 +42,11 @@ class Trip {
 	}
 
 	static async viewAlltrips(req, res) {
-		try {
-			const viewTrips = await TripModel.viewAlltrips();
-			if (!viewTrips) {
-				return reqResponses.handleError(404, 'No Trips record found', res);
-			}
-			return reqResponses.handleSuccess(200, 'success', viewTrips, res);
-		} catch (error) {
-			return reqResponses.internalError(res);
+		const viewTrips = await TripModel.viewAlltrips();
+		if (!viewTrips) {
+			return reqResponses.handleError(404, 'No Trips record found', res);
 		}
+		return reqResponses.handleSuccess(200, 'success', viewTrips, res);
 	}
 }
 
