3fb3f0ab1d36de452d2cb33b6369bfbf5fc81329
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-18 12:14:44 +0300

    ft(create-trip): add create trip endpoint route prefix [#167285194]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import user from './users';
+import trips from './trips';
 
 const apiPrefix = '/api/v1';
 const route = (app) => {
 	app.use(apiPrefix, user);
+	app.use(apiPrefix, trips);
 };
 
 export default route;
