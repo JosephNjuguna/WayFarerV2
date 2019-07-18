29d251121f7788e95a0731b60bf8e20d736dab68
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-18 12:16:57 +0300

    ft(create-trip): add trip mockdata for testing [#167285194]

---
+++ server/MockData/trips.js
@@ -0,0 +1,34 @@
+const trips = [
+	{
+		id: 1,
+		seatingCapacity: 14,
+		busLicensenumber: 'RAD 123',
+		origin: 'KIGALI',
+		destination: 'KAMPALA',
+		tripdate: '1/1/2019',
+		fare: 2000,
+		status: 'active',
+	},
+	{
+		id: 2,
+		seatingCapacity: 14,
+		busLicensenumber: 'RAD 456',
+		origin: 'KAMPALA',
+		destination: 'KIGALI',
+		tripdate: '2/2/2019',
+		fare: 2000,
+		status: 'active',
+	},
+	{
+		id: 3,
+		seatingCapacity: 14,
+		busLicensenumber: 'RAD 789',
+		origin: 'NAIROBI',
+		destination: 'KIGALI',
+		tripdate: '14/7/2019',
+		fare: 3000,
+		status: 'canceled',
+	},
+];
+
+export default trips;
