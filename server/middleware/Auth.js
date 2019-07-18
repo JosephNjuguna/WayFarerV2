bb027de15dea5a61205afc8dfd56db53c280de2f
JosephNjuguna <josephnjuguna482@gmail.com>
2019-07-18 12:18:45 +0300

    ft(create-trip): secure create trip endpoint [#167285194]

---
+++ server/middleware/Auth.js
@@ -0,0 +1,29 @@
+/* eslint-disable consistent-return */
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import reqResponses from '../helpers/Responses';
+
+dotenv.config();
+
+class AuthValidator {
+	static async checkAdmin(req, res, next) {
+		const tokenData = req.headers.authorization;
+		if (!tokenData || tokenData === null || tokenData === '') {
+			return reqResponses.handleError(400, 'Token required', res);
+		// eslint-disable-next-line no-else-return
+		} else {
+			const token = req.headers.authorization.split(' ')[1];
+			const decoded = jwt.verify(token, process.env.JWT_KEY);
+			req.userData = decoded;
+			if (req.userData.isAdmin === true) {
+				next();
+			} else {
+				return res.status(403).json({
+					message: 'Access Denied!',
+				});
+			}
+		}
+	}
+}
+
+export default AuthValidator;
