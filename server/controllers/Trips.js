679846b1561a74812a834582f2f0e9601efcaeab
JosephNjuguna <josephnjuguna482@gmail.com>
2019-08-14 00:25:23 +0300

    feat(trips) :
    admin view a single trip
    
    [Starts #167884152]

--- server/controllers/Trips.js
+++ server/controllers/Trips.js
@@ -63,7 +63,7 @@ class Trip {
 		const token = req.headers.authorization.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_KEY);
 		req.userData = decoded;
-		if (req.userData.isAdmin === true) {
+		if (req.userData.isadmin === 'true') {
 			const tripId = req.params.id;
 			const singleTrip = new TripModel(tripId);
 			if (!await singleTrip.viewSingletrip()) {
